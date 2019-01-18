e6ab33f40a7685273110727cadd00ee836ea25fb
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2019-01-19 00:43:48 +0100

    remove unnecessary layer

--- keyboards/hhkb/keymaps/fab_hhkb/keymap.c
+++ keyboards/hhkb/keymaps/fab_hhkb/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
             MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
             KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, MO(_FN),
-            MO(_FN), KC_LALT, LT(_MAC,KC_SPC), KC_ESC, KC_RGUI
+            MO(_FN), KC_LALT, KC_SPC, KC_ESC, KC_RGUI
             ),
 
     [_FN] = LAYOUT(
@@ -34,12 +34,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, _______, KC_MPLY, _______, _______, _______, _______, _______, _______, _______, KC_END, _______, _______,
             _______, _______, _______, _______, _______
             ),
-
-    /* [_MAC] = LAYOUT( */
-    /*         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, */
-    /*         _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_HOME, _______, KC_DEL, */
-    /*         KC_CAPS, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_PGDN, KC_PGUP, _______, */
-    /*         _______, _______, KC_MPLY, _______, _______, _______, _______, _______, _______, _______, KC_END, _______, _______, */
-    /*         KC_LGUI, _______, _______, _______, _______ */
-    /*         ), */
 };
