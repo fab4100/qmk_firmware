6598b703281f49f3500ffe3af5992cfd7758a5c0
Fabian Wermelinger <fab4100@posteo.net>
2023-12-31 14:57:51 +0100

    Fix space cadet and scroll lock keycodes

--- keyboards/dz60/keymaps/fab_tinaC_hhkb/keymap.c
+++ keyboards/dz60/keymaps/fab_tinaC_hhkb/keymap.c
@@ -19,6 +19,7 @@ enum CustomKeys
     RGB_HALO = SAFE_RANGE
 };
 
+// clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
             KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
             MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-            KC_LSPO, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, MO(_LIGHTS),
+            SC_LSPO, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SC_RSPC, MO(_LIGHTS),
             MO(_FN), KC_LALT, KC_SPC, KC_ESC, KC_RGUI
             ),
 
@@ -61,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_FN] = LAYOUT_true_hhkb(
             _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
-            KC_CAPS, KC_LCTL, KC_LALT, KC_LSFT, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_HOME, KC_END, KC_BSPC,
+            KC_CAPS, KC_LCTL, KC_LALT, KC_LSFT, _______, _______, _______, _______, KC_PSCR, KC_LSCR, KC_PAUS, KC_HOME, KC_END, KC_BSPC,
             MT(MOD_LCTL, KC_ESC), KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_PGDN, KC_PGUP, _______,
-            KC_LSPO, _______, _______, KC_MPLY, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+            SC_LSPO, _______, _______, KC_MPLY, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
             _______, KC_LALT, KC_SPC, KC_ESC, _______
             ),
 
@@ -86,11 +87,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______, _______, _______, _______, _______,
             RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_HALO, _______, _______, _______, _______, _______, _______, _______, _______, _______,
             _______, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            BL_TOGG, _______, BL_DEC, BL_INC, BL_ON , BL_OFF, BL_BRTG, _______, _______, _______, _______, _______, _______, _______,
+            BL_TOGG, _______, BL_DOWN, BL_UP, BL_ON , BL_OFF, BL_BRTG, _______, _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______
             ),
 };
 
+// clang-format on
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
