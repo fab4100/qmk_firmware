4a33f8700e595690465417cde8497b8575c99233
Fabian Wermelinger <info@0xfab.ch>
2025-03-03 21:11:09 +0100

    Move KC_CAPS to Umlaut layer

--- keyboards/dz60/keymaps/fab_tinaC_hhkb/keymap.c
+++ keyboards/dz60/keymaps/fab_tinaC_hhkb/keymap.c
@@ -43,16 +43,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN] = LAYOUT_true_hhkb(
             QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS,  KC_DEL,
             _______, KC_LCTL, KC_LALT, KC_LSFT, _______, _______, _______, _______, KC_PSCR, KC_LSCR, KC_PAUS, KC_HOME, KC_END, KC_BSPC,
-            KC_CAPS, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN, KC_PGUP, _______,
+            _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN, KC_PGUP, _______,
             _______, _______, _______, KC_MPLY, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
             _______, KC_LALT, KC_SPC,  KC_ESC,  _______
             ),
 
     [_LIGHTS] = LAYOUT_true_hhkb(
             RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______, _______, _______, _______, _______,
-            RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_HALO, _______,  _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, RGB_HUD, RGB_SAD, RGB_VAD, _______,  _______,  _______, _______, _______, _______, _______, _______, _______,
-            BL_TOGG, _______, BL_DOWN, BL_UP,   BL_ON ,   BL_OFF,   BL_BRTG, _______, _______, _______, _______, _______, _______, _______,
+            RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_HALO, _______,  _______, UML_UE,  _______, UML_OE,  _______, _______, _______, _______,
+            KC_CAPS, UML_AE,  RGB_SAD, RGB_VAD, _______,  _______,  _______, _______, _______, _______, _______, _______, _______,
+            BL_TOGG, RGB_HUD, BL_DOWN, BL_UP,   BL_ON ,   BL_OFF,   BL_BRTG, _______, _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______
             ),
 };
